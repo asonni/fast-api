14906e6bf2f2c2f178c966cf2cc936092f4f96cc
Aladdin Sonni <aladdin.sonni@gmail.com>
2024-04-07 02:53:17 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList
+} from '@/components/ui/command';
+
+export default function Home() {
+  const [input, setInput] = useState<string>('');
+  const [searchResults, setSearchResults] = useState<{
+    results: string[];
+    duration: number;
+  }>();
+
+  useEffect(() => {
+    (async () => {
+      if (!input) return setSearchResults(undefined);
+      // once deployed, prefix this with your cloudflare worker url
+      // i.e.: https://<name>.<account-name>.workers.dev/api/search?q=${input}
+
+      const res = await fetch(
+        `https://fast-api.asonni.workers.dev/api/search?q=${input}`
+      );
+      const data = (await res.json()) as {
+        results: string[];
+        duration: number;
+      };
+      setSearchResults(data);
+    })();
+  }, [input]);
+
+  return (
+    <main className="h-screen w-screen grainy">
+      <div className="flex flex-col gap-6 items-center pt-32 duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5">
+        <h1 className="text-5xl tracking-tight font-bold">SpeedSearch ⚡</h1>
+        <p className="text-zinc-600 text-lg max-w-prose text-center">
+          A high-performance API built with Hono, Next.js and Cloudflare. <br />{' '}
+          Type a query below and get your results in milliseconds.
+        </p>
+
+        <div className="max-w-md w-full">
+          <Command>
+            <CommandInput
+              value={input}
+              onValueChange={setInput}
+              placeholder="Search countries..."
+              className="placeholder:text-zinc-500"
+            />
+            <CommandList>
+              {searchResults?.results.length === 0 && (
+                <CommandEmpty>No results found.</CommandEmpty>
+              )}
+
+              {searchResults?.results && (
+                <CommandGroup heading="Results">
+                  {searchResults?.results.map(result => (
+                    <CommandItem
+                      key={result}
+                      value={result}
+                      onSelect={setInput}
+                    >
+                      {result}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              )}
+
+              {searchResults?.results && (
+                <>
+                  <div className="h-px w-full bg-zinc-200" />
+                  <p className="p-2 text-xs text-zinc-500">
+                    Found {searchResults.results.length} results in{' '}
+                    {searchResults?.duration.toFixed(0)}ms
+                  </p>
+                </>
+              )}
+            </CommandList>
+          </Command>
+        </div>
+      </div>
+    </main>
+  );
+}
